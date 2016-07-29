c97292699aa8d62b4af4f94e0288e5aac834b643
panicboy <asasaki@gmail.com>
2016-07-28 20:02:44 -1000

    merged

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+var PORT = 3000;
+
+
+
+
+
+
+
+
+var server = app.listen(portNum, function(){
+  var host = 'localhost';
+  console.log(`Example app listening at http://${host}:${portNum}`);
+});
