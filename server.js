3fda5c07905bb708cc92ed4d573ecaae162ad688
Tyler Chong <tylrchong@gmail.com>
2016-07-29 12:32:42 -1000

    Rendering cards

--- server.js
+++ server.js
@@ -19,16 +19,17 @@ var cardSchema = Schema({
   createdBy: String,
   assignedTo: String,
 });
-var Card = mongoose.model('Card',cardSchema);
+var Card = mongoose.model('Card', cardSchema);
 
-// app.use(methodOverride());
+app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static(__dirname)); // + './public'
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('/index.html');
+  console.log('test');
+  res.render('index');
 });
 
 app.post('/', (req, res) => {
