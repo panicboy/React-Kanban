43df548170366e2a7677304b36ef13c6800b59aa
Tyler Chong <tylrchong@gmail.com>
2016-07-29 19:44:53 -1000

    Sorted by columns

--- react/Column.jsx
+++ react/Column.jsx
@@ -9,17 +9,47 @@ var Column = React.createClass({
   },
   render() {
     var data = this.props.data;
-    var cards = data.map( (e) => {
-      return (
-        <Card data={e}/>
-      )
-    })
+    var cards = this.createByColumn(data);
     return (
-      <div className="column">
-        {cards}
+      <div>
+        <div id="Queue" className="column">
+          {cards[0]}
+        </div>
+        <div id="InProgress" className="column">
+          {cards[1]}
+        </div>
+        <div id="Done" className="column">
+          {cards[2]}
+        </div>
       </div>
     )
-  }
+  },
+  createByColumn(data) {
+    var one = [];
+    var two = [];
+    var three = [];
+    data.forEach( (e) => {
+      switch(e.status) {
+        case 'Queue':
+          one.push(
+            <Card data={e} />
+          )
+          break;
+        case 'InProgress':
+          two.push(
+            <Card data={e} />
+          )
+          break;
+        case 'Done':
+          three.push(
+            <Card data={e} />
+          )
+          break;
+        // case 'Done':
+      }
+    });
+    return [one, two, three];
+  },
 });
 
 export default Column;
