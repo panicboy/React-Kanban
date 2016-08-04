e30f69a4971be6bb5b113fca6e77c20c542427ad
Tyler Chong <tylrchong@gmail.com>
2016-08-03 15:43:39 -1000

    Refactored reducer

--- reducers/boardReducer.js
+++ reducers/boardReducer.js
@@ -18,44 +18,36 @@ var boardReducer = (state = initialState, action) => {
     case 'UPDATE_BOARD':
       return newState.set("data", action.data);
     case 'SHOW_FORM':
-      newState = newState.update("showForm", bool => bool = true);
-      return newState;
+      return newState.update("showForm", bool => bool = true);
     case 'HIDE_FORM':
-      newState = newState.update("showForm", bool => bool = false );
-      return newState;
+      return newState.update("showForm", bool => bool = false );
     case 'SHOW_EDIT_FORM_QUEUE':
-      newState = newState.update("showEditFormQueue", bool => bool = true);
-      newState = newState.set("editFormsBeingShown", 1);
-      newState = newState.set("showEditFormQueueState", action.status);
-      return newState;
+      return newStateShow(newState, 'Queue', action);
     case 'HIDE_EDIT_FORM_QUEUE':
-      newState = newState.update("showEditFormQueue", bool => bool = false);
-      newState = newState.set("showEditFormQueueState", {});
-      newState = newState.set("editFormsBeingShown", 0);
-      return newState;
+      return newStateHide(newState, 'Queue', action);
     case 'SHOW_EDIT_FORM_INPROGRESS':
-      newState = newState.update("showEditFormInProgress", bool => bool = true);
-      newState = newState.set("editFormsBeingShown", 1);
-      newState = newState.set("showEditFormInProgressState", action.status);
-      return newState;
+      return newStateShow(newState, 'InProgress', action);
     case 'HIDE_EDIT_FORM_INPROGRESS':
-      newState = newState.update("showEditFormInProgress", bool => bool = false);
-      newState = newState.set("showEditFormInProgressState", {});
-      newState = newState.set("editFormsBeingShown", 0);
-      return newState;
+      return newStateHide(newState, 'InProgress', action);
     case 'SHOW_EDIT_FORM_DONE':
-      newState = newState.update("showEditFormDone", bool => bool = true);
-      newState = newState.set("editFormsBeingShown", 1);
-      newState = newState.set("showEditFormDoneState", action.status);
-      return newState;
+      return newStateShow(newState, 'DONE', action);
     case 'HIDE_EDIT_FORM_DONE':
-      newState = newState.update("showEditFormDone", bool => bool = false);
-      newState = newState.set("showEditFormDoneState", {});
-      newState = newState.set("editFormsBeingShown", 0);
-      return newState;
+      return newStateHide(newState, 'DONE', action);
     default:
       return newState;
   }
 };
+function newStateShow(newState, status, action) {
+  newState = newState.update(`showEditForm${status}`, bool => bool = true);
+  newState = newState.set(`showEditForm${status}State`, action.status);
+  newState = newState.set("editFormsBeingShown", 1);
+  return newState;
+}
+function newStateHide(newState, status, action) {
+  newState = newState.update(`showEditForm${status}`, bool => bool = false);
+  newState = newState.set(`showEditForm${status}State`, {});
+  newState = newState.set("editFormsBeingShown", 0);
+  return newState;
+}
 
 export default boardReducer;
